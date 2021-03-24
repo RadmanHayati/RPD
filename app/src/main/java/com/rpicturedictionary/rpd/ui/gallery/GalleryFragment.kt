bca1dee51045ea9755a1dbd18b5b6d138369da52
Radman Hayati <radman.hayatii@gmail.com>
2021-03-24 22:33:40 +0430

    Fragment is ready.

--- app/src/main/java/com/rpicturedictionary/rpd/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/rpicturedictionary/rpd/ui/gallery/GalleryFragment.kt
@@ -1,9 +1,19 @@
 package com.rpicturedictionary.rpd.ui.gallery
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.rpicturedictionary.rpd.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+    private val viewModel by viewModels<GalleryViewModel>()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+      viewModel.photos.observe(viewLifecycleOwner){
+
+      }
+    }
+
 }
