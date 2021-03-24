a36a4d7a47efcd3052fd5b00dad136dfe9ca1be2
Radman Hayati <radman.hayatii@gmail.com>
2021-03-24 22:17:33 +0430

    I'm done with the repository

--- app/src/main/java/com/rpicturedictionary/rpd/data/UnsplashRepository.kt
+++ app/src/main/java/com/rpicturedictionary/rpd/data/UnsplashRepository.kt
@@ -1,5 +1,8 @@
 package com.rpicturedictionary.rpd.data
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
 import com.rpicturedictionary.rpd.api.UnsplashApi
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -7,4 +10,13 @@ import javax.inject.Singleton
 @Singleton
 class UnsplashRepository @Inject constructor(private val unsplashApi: UnsplashApi) {
 
+    fun getSearchResult(query: String) =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20, //params.loadSize() how many items fit on screen
+                maxSize = 100,
+                enablePlaceholders = false,
+            ),
+            pagingSourceFactory = { UnsplashPagingSource(unsplashApi, query) }
+        ).liveData
 }
