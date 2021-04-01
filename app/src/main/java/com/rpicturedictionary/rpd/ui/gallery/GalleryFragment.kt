7b7b00ffec921cbfb89b5a5d25cf201a935dfcf9
Radman Hayati <radman.hayatii@gmail.com>
2021-04-02 00:19:07 +0430

    handling states

--- app/src/main/java/com/rpicturedictionary/rpd/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/rpicturedictionary/rpd/ui/gallery/GalleryFragment.kt
@@ -7,8 +7,10 @@ import android.view.MenuInflater
 import android.view.View
 import android.widget.SearchView
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.paging.LoadState
 import com.rpicturedictionary.rpd.R
 import com.rpicturedictionary.rpd.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -26,24 +28,50 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
         binding.apply {
             recyclerView.setHasFixedSize(true)
             recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
-                header = UnsplashPhotoLoadStateAdapter{adapter.retry()},
-                footer = UnsplashPhotoLoadStateAdapter{adapter.retry()}
+                header = UnsplashPhotoLoadStateAdapter { adapter.retry() },
+                footer = UnsplashPhotoLoadStateAdapter { adapter.retry() }
             )
+            buttonRetry.setOnClickListener {
+                adapter.retry()
+            }
         }
         viewModel.photos.observe(viewLifecycleOwner) {
-             adapter.submitData(viewLifecycleOwner.lifecycle, it)
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
+        adapter.addLoadStateListener { loadState ->
+            binding.apply {
+                progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+                recyclerView.isVisible = loadState.source.refresh is LoadState.NotLoading
+                buttonRetry.isVisible = loadState.source.refresh is LoadState.Error
+                textViewError.isVisible = loadState.source.refresh is LoadState.Error
+
+           // No result
+            if(loadState.source.refresh is LoadState.NotLoading &&
+                    loadState.append.endOfPaginationReached &&
+                        adapter.itemCount < 1){
+
+                recyclerView.isVisible= false
+                textViewEmpty.isVisible = true
+
+            } else {
+                textViewEmpty.isVisible = false
+            }
+
+            }
+        }
+
         setHasOptionsMenu(true)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.menu_gallery,menu)
-        val searchItem=menu.findItem(R.id.action_search)
-        val searchView=searchItem.actionView as androidx.appcompat.widget.SearchView
-        searchView.setOnQueryTextListener(object :androidx.appcompat.widget.SearchView.OnQueryTextListener{
+        inflater.inflate(R.menu.menu_gallery, menu)
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as androidx.appcompat.widget.SearchView
+        searchView.setOnQueryTextListener(object :
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                if (query !=null) {
+                if (query != null) {
                     binding.recyclerView.scrollToPosition(0)
                     viewModel.searchPhotos(query)
                     searchView.clearFocus()
@@ -52,13 +80,14 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-              return true
+                return true
             }
 
         })
 
 
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
