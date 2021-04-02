bbf97f9492a31e6dab407a615f36f4cc65f43a2c
Radman Hayati <radman.hayatii@gmail.com>
2021-04-02 14:54:58 +0430

    navigation to DetailsFragment.kt

---
+++ app/src/main/java/com/rpicturedictionary/rpd/ui/details/DetailsFragment.kt
@@ -0,0 +1,7 @@
+package com.rpicturedictionary.rpd.ui.details
+
+import androidx.fragment.app.Fragment
+import com.rpicturedictionary.rpd.R
+
+class DetailsFragment : Fragment(R.layout.fragment_details){
+}
