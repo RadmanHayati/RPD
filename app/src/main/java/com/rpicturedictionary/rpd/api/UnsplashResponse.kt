5101a0525245f0aae3e438663f4f4677f8366c58
Radman Hayati <radman.hayatii@gmail.com>
2021-04-03 18:03:44 +0430

    DetailsFragment.kt implementation completed

--- app/src/main/java/com/rpicturedictionary/rpd/api/UnsplashResponse.kt
+++ app/src/main/java/com/rpicturedictionary/rpd/api/UnsplashResponse.kt
@@ -2,6 +2,6 @@ package com.rpicturedictionary.rpd.api
 
 import com.rpicturedictionary.rpd.data.UnsplashPhoto
 
-data class UnsplashResponse(
+data class UnsplashResponse(  // unsplash API data class which is a list of UnsplashPhoto data class
     val results: List<UnsplashPhoto>
 )
